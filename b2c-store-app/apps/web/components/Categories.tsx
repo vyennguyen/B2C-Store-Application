2dd70cb391530369ee752a6c8998b938f92679fe
Vy Nguyen <22046048@student.westernsydney.edu.au>
2025-05-18 14:56:03 +1000

    Migrated the project to a monorepo

---
+++ b2c-store-app/apps/web/components/Categories.tsx
@@ -0,0 +1,5 @@
+function Categories() {
+  return <div aria-label="Categories" className="flex"></div>;
+}
+
+export default Categories;
---
+++ b2c-store-app/apps/web/components/NavBar.tsx
@@ -0,0 +1,10 @@
+function NavBar() {
+  return (
+    <>
+      <div aria-label="Logo">Logo</div>
+      <div></div>
+    </>
+  );
+}
+
+export default NavBar;
