82eb8aa03633b6039177c37c86bf8df624bee2d4
Vy Nguyen <22046048@student.westernsydney.edu.au>
2025-05-20 14:55:20 +1000

    Content for homepage

--- b2c-app/src/app/components/Main.tsx
+++ b2c-app/src/app/components/Main.tsx
@@ -1,5 +1,9 @@
 "use client";
 
 export default function Main() {
-  return <main className="flex min-h-screen"></main>;
+  return (
+    <main className="flex min-h-screen">
+      <div aria-label="Home Page">Some content here</div>
+    </main>
+  );
 }
