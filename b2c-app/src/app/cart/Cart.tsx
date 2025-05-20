4699a9289cf9261b420e761677f5217090d4de74
Vy Nguyen <22046048@student.westernsydney.edu.au>
2025-05-20 15:42:59 +1000

    Added cart

---
+++ b2c-app/src/app/cart/Cart.tsx
@@ -0,0 +1,12 @@
+export default function Cart() {
+  return (
+    <div className="flex min-h-screen">
+      <div className="w-[25%]">
+        <nav aria-label="Left Menu"></nav>
+      </div>
+      <div className="w-[75%] overflow-auto">
+        <div aria-label="Cart Page">Some content here</div>
+      </div>
+    </div>
+  );
+}
