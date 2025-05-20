ab0801abd30d75e8f5f00ef03b869ee375c84f62
Vy Nguyen <22046048@student.westernsydney.edu.au>
2025-05-20 13:58:00 +1000

    Restyling

--- b2c-app/src/app/products/ProductPage.tsx
+++ b2c-app/src/app/products/ProductPage.tsx
@@ -6,7 +6,7 @@ import { mockProducts } from "../data/products";
 function ProductPage() {
   return (
     <div className="min-h-screen p-5">
-      <h1 className="text-lg font-bold mb-5">All keyboards</h1>
+      <h1 className="text-lg font-bold mb-5">All Keyboards</h1>
       <div className="flex flex-wrap gap-4 justify-center">
         {mockProducts.map((product) => (
           <ProductCard
