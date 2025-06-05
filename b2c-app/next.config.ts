70d1ba8897e17287508d48767789b682969cc6bc
Vy Nguyen <22046048@student.westernsydney.edu.au>
2025-06-05 10:59:56 +1000

    Added domain and new product image

--- b2c-app/next.config.ts
+++ b2c-app/next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         hostname: "images.unsplash.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        pathname: "/**",
+      },
     ],
   },
 };
